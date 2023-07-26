776dd1dce516a57c5deb3b7a0ae27608efc06ddd
beto <beto@localhost.localdomain>
2023-07-25 22:03:47 -0300

    Home updated

--- album.php
+++ album.php
@@ -1,44 +1,47 @@
+
+
 <?php
+    include "./index.html";
+?>    
 
-include "./index.html";
+<?php
+    try {
+        if (extension_loaded('mysqli')) {
+            // echo 'La extensión MySQLi está habilitada en este servidor.';
 
-try {
-    if (extension_loaded('mysqli')) {
-        // echo 'La extensión MySQLi está habilitada en este servidor.';
-
-        // Realizar la solicitud a la BBDD
-        $connection = mysqli_connect('localhost', 'alber', '1234', 'Chinook');
-        if (!$connection) {
-            die("La conexión falló :( : " . mysqli_connect_error());
-        } else {
-            $query = "SELECT * FROM Album";
-            $result = mysqli_query($connection, $query);      
+            // Realizar la solicitud a la BBDD
+            $connection = mysqli_connect('localhost', 'alber', '1234', 'Chinook');
+            if (!$connection) {
+                die("La conexión falló :( : " . mysqli_connect_error());
+            } else {
+                $query = "SELECT * FROM Album";
+                $result = mysqli_query($connection, $query);      
         
 
 ?>
-        <center>
-            <?php
-            echo "<h2>Albums</h2>";
+                <center>
+                    <?php
+                    echo "<h2>Albums</h2>";
 
-            echo "<table border='1'>";
-            echo "<tr><th>ID</th><th>Title</th><th>Artist ID</th></tr>";
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<tr><td>" . $row["AlbumId"] . "</td><td>" . $row["Title"] . "</td><td>" .  $row["ArtistId"] . "</td></tr>";
-            }
-            echo "</table>";
+                    echo "<table border='1'>";
+                    echo "<tr><th>ID</th><th>Title</th><th>Artist ID</th></tr>";
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<tr><td>" . $row["AlbumId"] . "</td><td>" . $row["Title"] . "</td><td>" .  $row["ArtistId"] . "</td></tr>";
+                    }
+                    echo "</table>";
 
-            ?>
-        </center>
+                    ?>
+                </center>
 <?php
+            }
+        } else {
+            echo 'La extensión MySQLi no está habilitada en este servidor.';
         }
-    } else {
-        echo 'La extensión MySQLi no está habilitada en este servidor.';
+    } catch (Exception $e) {
+        echo $e->getMessage();
     }
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
 
-mysqli_close($connection);
+    mysqli_close($connection);
 ?>
 
 <script>
---
+++ employee.php
@@ -0,0 +1,77 @@
+<div>
+    <?php
+
+        include "./index.html";
+
+        try{
+            if(extension_loaded('mysqli')){
+                $connection = mysqli_connect('localhost', 'alber', '1234', 'Chinook');
+                
+                if(!$connection) {
+                    echo "Connection failed :(";
+
+                } else {
+                    $query = "SELECT * FROM Employee";                    
+                    $result = mysqli_query($connection, $query);
+
+                    if(!$result) {
+                        echo "Query in incorrect :(";
+                    } else {
+                        echo "<table border=1>";
+                        echo "<tr>";
+                        echo "<th>ID</th>";
+                        echo "<th>First Name</th>";
+                        echo "<th>Last Name</th>";
+                        echo "<th>Title</th>";
+                        echo "<th>Reports To</th>";
+                        echo "<th>Birthdate</th>";
+                        echo "<th>Hire date</th>";
+                        echo "<th>Address</th>";
+                        echo "<th>City</th>";
+                        echo "<th>State</th>";
+                        echo "<th>Country</th>";
+                        echo "<th>Postal Code</th>";
+                        echo "<th>Phone</th>";
+                        echo "<th>Fax</th>";
+                        echo "<th>Email</th>";
+                        echo "</th>";
+                        echo "</tr>";
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td>" . $row["EmployeeId"] . "</td>"; 
+                            echo "<td>" . $row["FirstName"] . "</td>";
+                            echo "<td>" . $row["LastName"] . "</td>";
+                            echo "<td>" . $row["Title"] . "</td>";
+                            echo "<td>" . $row["Reports To"] . "</td>";
+                            echo "<td>" . $row["BirthDate"] . "</td>";
+                            echo "<td>" . $row["HireDate"] . "</td>";
+                            echo "<td>" . $row["Address"] . "</td>";
+                            echo "<td>" . $row["City"] . "</td>";
+                            echo "<td>" . $row["State"] . "</td>";
+                            echo "<td>" . $row["Country"] . "</td>";
+                            echo "<td>" . $row["PostalCode"] . "</td>";
+                            echo "<td>" . $row["Phone"] . "</td>";
+                            echo "<td>" . $row["Fax"] . "</td>";
+                            echo "<td>" . $row["Email"] . "</td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                    }
+                }
+
+
+
+
+            } else {
+                echo "You have to enable 'mysqli' extension";
+            }
+
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+
+        mysqli_close($connection);
+    
+    ?>
+
+</div>
